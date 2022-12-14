c606957d00cac08a1ee22b2a2c9afd58074703af
Thomas Alexander Gerds <tag@biostat.ku.dk>
2022-12-14 13:20:43 +0100

    typos

--- exercises/statins/_targets.R
+++ exercises/statins/_targets.R
@@ -3,9 +3,9 @@
 ## Author: Thomas Alexander Gerds
 ## Created: Dec  7 2022 (11:18) 
 ## Version: 
-## Last-Updated: Dec 12 2022 (08:02) 
+## Last-Updated: Dec 14 2022 (13:19) 
 ##           By: Thomas Alexander Gerds
-##     Update #: 9
+##     Update #: 10
 #----------------------------------------------------------------------
 ## 
 ### Commentary: 
@@ -30,7 +30,20 @@ for (f in list.files("functions/",
         x <- Ltmle(data=dd,Qform=c("CVD_1"="Q.kplus1 ~ sex + age + ldl + Statins_0 + Statins_1","CVD_2"="Q.kplus1 ~ sex + age + ldl + Statins_0 + Statins_1"),gform=c("Statins_0"="Statins_0 ~ sex + age + ldl","Statins_1"="Statins_1 ~ sex + age + ldl + Statins_0","Censored_1"="Censored_1 ~ sex + age + ldl + Statins_0 + Statins_1","Statins_2"= "Statins_2 ~ sex + age + ldl + Statins_0 + Statins_1"),Anodes=c("Statins_0","Statins_1","Statins_2"),Lnodes=c("sex","age","ldl"),Ynodes=c("CVD_1","CVD_2"),Cnodes=c("Censored_1"),estimate.time=FALSE,survivalOutcome=TRUE,variance.method="ic",SL.library="glm",abar=c(1,1,1))
         y <- Ltmle(data=dd,Qform=c("CVD_1"="Q.kplus1 ~ sex + age + ldl + Statins_0 + Statins_1","CVD_2"="Q.kplus1 ~ sex + age + ldl + Statins_0 + Statins_1"),gform=c("Statins_0"="Statins_0 ~ sex + age + ldl","Statins_1"="Statins_1 ~ sex + age + ldl + Statins_0","Censored_1"="Censored_1 ~ sex + age + ldl + Statins_0 + Statins_1","Statins_2"= "Statins_2 ~ sex + age + ldl + Statins_0 + Statins_1"),Anodes=c("Statins_0","Statins_1","Statins_2"),Lnodes=c("sex","age","ldl"),Ynodes=c("CVD_1","CVD_2"),Cnodes=c("Censored_1"),estimate.time=FALSE,survivalOutcome=TRUE,variance.method="ic",SL.library="glm",abar=c(0,0,0))
         w <- Ltmle(data=dd,Qform=c("CVD_1"="Q.kplus1 ~ sex + age + ldl + Statins_0 + Statins_1","CVD_2"="Q.kplus1 ~ sex + age + ldl + Statins_0 + Statins_1"),gform=c("Statins_0"="Statins_0 ~ sex + age + ldl","Statins_1"="Statins_1 ~ sex + age + ldl + Statins_0","Censored_1"="Censored_1 ~ sex + age + ldl + Statins_0 + Statins_1","Statins_2"= "Statins_2 ~ sex + age + ldl + Statins_0 + Statins_1"),Anodes=c("Statins_0","Statins_1","Statins_2"),Lnodes=c("sex","age","ldl"),Ynodes=c("CVD_1","CVD_2"),Cnodes=c("Censored_1"),estimate.time=FALSE,survivalOutcome=TRUE,variance.method="ic",SL.library="glm",abar=list(c(1,1,1),c(0,0,0)))
-        W <- Ltmle(data=dd,Qform=c("CVD_1"="Q.kplus1 ~ sex + age + ldl + Statins_0 + Statins_1","CVD_2"="Q.kplus1 ~ sex + age + ldl + Statins_0 + Statins_1"),gform=c("Statins_0"="Statins_0 ~ sex + age + ldl","Statins_1"="Statins_1 ~ sex + age + ldl + Statins_0","Censored_1"="Censored_1 ~ sex + age + ldl + Statins_0 + Statins_1","Statins_2"= "Statins_2 ~ sex + age + ldl + Statins_0 + Statins_1"),Anodes=c("Statins_0","Statins_1","Statins_2"),Lnodes=c("sex","age","ldl"),Ynodes=c("CVD_1","CVD_2"),Cnodes=c("Censored_1"),estimate.time=FALSE,survivalOutcome=TRUE,variance.method="ic",SL.library="glmnet",abar=list(c(1,1,1),c(0,0,0)),SL.cvControl=list(alpha=0.5,selector='undersmooth'),verbose=TRUE)
+        W <- Ltmle(data=dd,
+                   Qform=c("CVD_1"="Q.kplus1 ~ sex + age + ldl + Statins_0 + Statins_1","CVD_2"="Q.kplus1 ~ sex + age + ldl + Statins_0 + Statins_1"),
+                   gform=c("Statins_0"="Statins_0 ~ sex + age + ldl","Statins_1"="Statins_1 ~ sex + age + ldl + Statins_0","Censored_1"="Censored_1 ~ sex + age + ldl + Statins_0 + Statins_1","Statins_2"= "Statins_2 ~ sex + age + ldl + Statins_0 + Statins_1"),
+                   Anodes=c("Statins_0","Statins_1","Statins_2"),
+                   Lnodes=c("sex","age","ldl"),
+                   Ynodes=c("CVD_1","CVD_2"),
+                   Cnodes=c("Censored_1"),
+                   estimate.time=FALSE,
+                   survivalOutcome=TRUE,
+                   variance.method="ic",
+                   SL.library="glmnet",
+                   abar=list(c(1,1,1),c(0,0,0)),
+                   SL.cvControl=list(alpha=0.5,selector='undersmooth'),
+                   verbose=TRUE)
         publish(w)
         publish(W)
     })
