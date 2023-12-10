f25b2c831cb14d50531dda873e6babce16fe1d99
Thomas Alexander Gerds <tag@biostat.ku.dk>
2023-12-10 13:27:33 +0100

    the 2023 edition

--- exercises/register_project/sandbox.R
+++ exercises/register_project/sandbox.R
@@ -3,9 +3,9 @@
 ## Author: Thomas Alexander Gerds
 ## Created: Dec  8 2023 (19:05) 
 ## Version: 
-## Last-Updated: Dec 10 2023 (09:04) 
+## Last-Updated: Dec 10 2023 (13:23) 
 ##           By: Thomas Alexander Gerds
-##     Update #: 11
+##     Update #: 13
 #----------------------------------------------------------------------
 ## 
 ### Commentary: 
@@ -50,6 +50,26 @@ tar_load(table_ltmle_death_2)
 #--------------------------------------------------------
 # day 3
 #--------------------------------------------------------
+
+# part 3
+library(glmnet)
+tar_load_everything()
+X=model.matrix(~ -1 + sex + education + agegroups + tertile_income + index_heart_failure + diabetes_duration,
+               data = register_data)
+Y=register_data[["Drug_0"]]
+set.seed(3)
+lasso <- cv.glmnet(x = X,y = Y,alpha = 1)
+coef(lasso)
+ridge <- cv.glmnet(x = X,y = Y,alpha = 0)
+coef(ridge)
+elnet <- cv.glmnet(x = X,y = Y,alpha = 0.5)
+coef(elnet)
+
+# with some interactions
+XX=model.matrix(~ -1 + sex * agegroups + education * tertile_income + index_heart_failure * diabetes_duration,
+                data = register_data)
+elnet_interaction <- cv.glmnet(x = XX,y = Y,alpha = 0.5)
+coef(elnet_interaction)
 
 #--------------------------------------------------------
 # day 4
