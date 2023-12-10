acdcde2c05be0fc4e50d4972561692e8964be3f0
Thomas Alexander Gerds <tag@biostat.ku.dk>
2023-12-10 08:18:50 +0100

    the 2023 edition

--- exercises/register_project/sandbox.R
+++ exercises/register_project/sandbox.R
@@ -3,9 +3,9 @@
 ## Author: Thomas Alexander Gerds
 ## Created: Dec  8 2023 (19:05) 
 ## Version: 
-## Last-Updated: Dec  9 2023 (12:25) 
+## Last-Updated: Dec 10 2023 (08:14) 
 ##           By: Thomas Alexander Gerds
-##     Update #: 3
+##     Update #: 9
 #----------------------------------------------------------------------
 ## 
 ### Commentary: 
@@ -15,17 +15,34 @@
 ## 
 ### Code:
 library(targets)
-## tar_make()
+tar_source("functions")
+tar_make()
+tar_manifest()
+tar_visnetwork()
+show_performance()
+show_warnings()
 tar_load_everything()
 
+#--------------------------------------------------------
 # day 2
+#--------------------------------------------------------
 
+# part 1
+tar_load(raw_baseline_covariates)
+print(raw_baseline_covariates)
 
+tar_make()
+tar_load(baseline_covariates)
+
+# adapt the table_1 target defined in example_project (yesterday exercise)
+
+#--------------------------------------------------------
 # day 3
+#--------------------------------------------------------
 
-
+#--------------------------------------------------------
 # day 4
-
+#--------------------------------------------------------
 
 
 ######################################################################
