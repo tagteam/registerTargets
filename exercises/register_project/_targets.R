1602606d5989075c6cdf45461f0edd285d430eba
Thomas Alexander Gerds <tag@biostat.ku.dk>
2023-12-12 10:06:13 +0100

    the 2023 edition

--- exercises/register_project/_targets.R
+++ exercises/register_project/_targets.R
@@ -3,9 +3,9 @@
 ## Author: Thomas Alexander Gerds
 ## Created: Dec  7 2023 (17:32) 
 ## Version: 
-## Last-Updated: Dec 12 2023 (10:04) 
+## Last-Updated: Dec 12 2023 (10:05) 
 ##           By: Thomas Alexander Gerds
-##     Update #: 15
+##     Update #: 16
 #----------------------------------------------------------------------
 ## 
 ### Commentary:
@@ -30,7 +30,7 @@ tar_option_set(packages=c("prodlim",
                           "Publish"))
 # Definition of project functions
 tar_source("functions")
-source("../Ltmle/Ltmle.R")
+source("../Ltmle")
 
 list(
     tar_target(register_data, fread("data/register_data.csv")),
